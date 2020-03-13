ac75e53fdd0e4a889b47953c2c6908139da2bfe1
udiEli1997 <udisteanu.iulian@outlook.com>
2020-03-13 22:46:23 +0200

    Adaugare clasa Persoana

---
+++ Agenda/Agenda/Persoana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    enum Grup{ Prieteni,Servici,Familie };
+    class Persoana
+    {
+        string nume;
+        string prenume;
+        string email;
+        string numar_telegon;
+        Grup grup;
+
+        public Persoana(string _nume,string _prenume,string _email,string _numar,Grup _grup)
+        {
+            nume = _nume;
+            prenume = _prenume;
+            email = _email;
+            numar_telegon = _numar;
+            grup = _grup;
+        }
+
+    }
+}
